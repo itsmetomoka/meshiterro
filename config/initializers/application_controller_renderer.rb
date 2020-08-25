090c7ad6a0d4f0e26211b238b30888077da9969e
Hara Tomoka <pixieyunomi@gmail.com>
2020-08-25 14:13:00 +0900

    [finish]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4a09b1a32a44912e60fc94b3e851e6c2f4b2025704efe2f69549ea37507c49e4464a07e3baec6154e9023ff10cac81e3e0683489d860c77a2cd065107dab61a6'
